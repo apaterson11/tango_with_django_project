cd9ca3850e448338fa20018609ef75e3ef2dcc87
Alex Paterson <2382854P@student.gla.ac.uk>
2020-01-30 16:57:44 +0000

    rolled back to commit f4ad6c40f317be53a3354b7fd4e024c4eaf37892 as tests for chapter 3 did not work

--- rango/views.py
+++ rango/views.py
@@ -3,22 +3,7 @@ from django.http import HttpResponse
 from html.parser import HTMLParser
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
-
+    return HttpResponse("<a href='/rango/about/'>About</a>")
     
 def about(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'This tutorial has been put together by Alex Paterson.'}
-    
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/about.html', context=context_dict)
+    return HttpResponse("<a href = '/rango/'>Index</a>")
