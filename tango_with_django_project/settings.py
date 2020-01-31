2aaa3f0fda99283f68f5528b4f17b97890ba713b
Alex Paterson <2382854P@student.gla.ac.uk>
2020-01-31 00:26:22 +0000

    completed chapter 5, test passed

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+TIME_ZONE = 'GMT'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
